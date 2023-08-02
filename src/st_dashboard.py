aa9478c161f41451a28a70fb7a0c9e175a3a5230
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2023-08-02 15:41:44 -0500

    Got the plot working.

--- src/st_dashboard.py
+++ src/st_dashboard.py
@@ -50,7 +50,7 @@ plot_context = plotting_setup()
 # Load data
 ################################################################################
 
-st.title( 'Articles Per Year' )
+st.title( 'Articles Per Year for CIERA' )
 
 # User data selection
 group_by = st.selectbox( 'Select what you want to group the articles by:', config['groupings'] )
@@ -92,12 +92,98 @@ all_selected_columns = []
 for i, group_by_i in enumerate( remaining_groupings ):
     possible_columns = pd.unique( exploded.index.get_level_values(i) )
     selected_columns = st.multiselect( group_by_i, possible_columns, default=possible_columns )
+    if len( selected_columns ) == 0:
+        st.error( 'You must select at least one option.' )
+        st.stop()
     all_selected_columns.append( selected_columns )
 
 is_included = np.ones( len( exploded ), ).astype( bool )
 for i, group_by_i in enumerate( remaining_groupings ):
     is_included = is_included & exploded.index.isin( all_selected_columns[i], level=i )
 selected = exploded.loc[is_included]
+
+# Get counts
+counts = selected.pivot_table( values='id', index='Year', columns='Categories', aggfunc='nunique' )
+
+# Replace "None"s
+counts.fillna( value=0, inplace=True )
+
+################################################################################
+# Plot
+################################################################################
+
+years = counts.index
+categories = st.multiselect( group_by, counts.columns, default=list(counts.columns) )
+
+# Sidebar for figure tweaks
+st.sidebar.markdown( '# Figure Tweaks' )
+fig_width, fig_height = matplotlib.rcParams['figure.figsize']
+plot_kw = {
+    'fig_width': st.sidebar.slider( 'figure width', 0.1*fig_width, 2.*fig_width, value=9. ),
+    'fig_height': st.sidebar.slider( 'figure height', 0.1*fig_height, 2.*fig_height, value=fig_height ),
+    'font_scale': st.sidebar.slider( 'font scale', 0.1, 2.0, value=1. ),
+    'legend_x': st.sidebar.slider( 'legend x', 0., 1., value=0. ),
+    'legend_y': st.sidebar.slider( 'legend y', 0., 1.5, value=1. ),
+    'tick_spacing': st.sidebar.slider( 'y tick spacing', 1, 10, value=int(np.ceil(counts.values.max()/30.)) )
+}
+
+@st.cache_data
+def plot_counts( group_by, all_selected_columns, categories, plot_kw ):
+
+    fig = plt.figure( figsize=( plot_kw['fig_width'], plot_kw['fig_height'] ) )
+    ax = plt.gca()
+    for j, category_j in enumerate( categories ):
+
+        ax.plot(
+            years,
+            counts[category_j],
+            linewidth = 2,
+            alpha = 0.5,
+        )
+        ax.scatter(
+            years,
+            counts[category_j],
+            label = category_j,
+        )
+
+    ymax = ax.get_ylim()[1]
+
+    ax.set_xticks( years )
+    count_ticks = np.arange( 0, ymax, plot_kw['tick_spacing'] )
+    ax.set_yticks( count_ticks )
+
+    ax.set_xlim( years[0], years[-1] )
+    ax.set_ylim( 0, ymax )
+
+    l = ax.legend(
+        bbox_to_anchor = ( plot_kw['legend_x'], plot_kw['legend_y'] ),
+        loc = 'lower left', 
+        framealpha = 1.,
+        fontsize = plot_context['legend.fontsize'] * plot_kw['font_scale'],
+        ncol = len( categories ) // 4 + 1
+    )
+
+    ax.set_xlabel( 'Year', fontsize=plot_context['axes.labelsize'] * plot_kw['font_scale'] )
+    ax.set_ylabel( 'Count', fontsize=plot_context['axes.labelsize'] * plot_kw['font_scale'] )
+
+    ax.tick_params( labelsize=plot_context['xtick.labelsize']*plot_kw['font_scale'] )
+
+    # return facet_grid
+    return fig
+fig = plot_counts( group_by, all_selected_columns, categories, plot_kw )
+st.pyplot( fig )
+
+# Add a download button for the image
+fn = 'counts.{}.pdf'.format( group_by )
+img = io.BytesIO()
+fig.savefig( img, format='pdf', bbox_inches='tight' )
+st.download_button(
+    label="Download Figure",
+    data=img,
+    file_name=fn,
+    mime="image/pdf"
+)
+
 
 ################################################################################
 # Display Raw Data
