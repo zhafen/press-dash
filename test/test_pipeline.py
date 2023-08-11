52ffed1bc632f4b74c8902e80bdb3769cdbb7b97
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2023-08-11 18:11:00 -0500

    Working on refactoring to enable simple use of config.

--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -1,12 +1,11 @@
 import unittest
 
+import datetime
 import glob
 import os
 import shutil
 import subprocess
 import yaml
-
-from press_dashboard_library import pipeline
 
 ###############################################################################
 
@@ -16,15 +15,16 @@ class TestPipeline( unittest.TestCase ):
 
         # Get filepath info
         test_dir = os.path.abspath( os.path.dirname( __file__ ) )
-        self.config_fp = os.path.join( test_dir, 'config_raw_only.yml' )
         self.root_dir = os.path.dirname( test_dir )
         self.test_data_dir = os.path.join( self.root_dir, 'test_data', 'test_data_raw_only' )
+        self.config_fp = os.path.join( self.test_data_dir, 'config.yml' )
+        self.timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
         # Set up temporary dirs
         self.temp_dirs = {
             'processed_data_dir': os.path.join( self.test_data_dir, 'processed_data' ),
             'figure_dir': os.path.join( self.test_data_dir, 'figures' ),
-            'logs_dir': os.path.join( self.test_data_dir, 'logs' ),
+            'logs_dir': os.path.join( self.root_dir, 'logs' ),
         }
         for key, temp_dir in self.temp_dirs.items():
             if os.path.isdir( temp_dir ):
@@ -39,7 +39,7 @@ class TestPipeline( unittest.TestCase ):
     def tearDown( self ):
 
         # Remove dashboard and figures temp dirs
-        for key in self.temp_dirs.keys():
+        for key in [ 'processed_data_dir', 'figure_dir' ]:
             temp_dir = self.temp_dirs[key]
             if os.path.isdir( temp_dir ):
                 shutil.rmtree( temp_dir )
@@ -64,7 +64,27 @@ class TestPipeline( unittest.TestCase ):
     def test_transform( self ):
         '''Test that transform works'''
 
-        pipeline.transform( self.config_fp )
+        # Move to the config directory
+        os.chdir( os.path.dirname( self.config_fp ) )
+
+        nb_fp = os.path.join( self.root_dir, 'src', 'transform.ipynb' )
+        command = ' '.join( (
+            'jupyter',
+            'nbconvert',
+            '--to notebook',
+            '--execute {}'.format( nb_fp ),
+            '--output=transform.{}.test.ipynb'.format( self.timestamp ),
+            '--output-dir={}'.format( self.temp_dirs['logs_dir'] ),
+        ) )
+        subprocess_output = subprocess.run(
+            command,
+            shell=True,
+            capture_output=True,
+        )
+
+        # Ensure it ran successfully
+        # os.system( command )
+        assert subprocess_output.returncode == 0
 
         # Check that there are output files
         output_files = [
@@ -90,23 +110,7 @@ class TestPipeline( unittest.TestCase ):
         # Make an extra copy
         shutil.copy( self.news_data_fp, self.dup_news_data_fp )
 
-        pipeline.transform( self.config_fp )
-
-        # Check that there are output files
-        output_files = [
-            ( 'counts', 'counts.categories.csv', ),
-            ( 'counts', 'counts.press_types.csv', ),
-            ( 'counts', 'counts.research_topics.csv', ),
-            ( 'press.csv', ),
-            ( 'press.exploded.csv', ),
-        ]
-        for output_file in output_files:
-            output_fp = os.path.join( self.temp_dirs['processed_data_dir'], *output_file )
-            assert os.path.isfile( output_fp )
-
-        # Check that there's an output NB in the logs
-        transform_fps = glob.glob( os.path.join( self.temp_dirs['logs_dir'], 'transform_*.ipynb' ) )
-        assert len( transform_fps ) > 0
+        self.test_transform()
 
     ###############################################################################
 
